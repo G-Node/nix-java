02acc29a8b73b0ac540b8de2dbc23f58e9664129
sujith <sujithvm24@gmail.com>
2015-06-13 23:58:21 +0530

    fix data array test

--- src/test/java/org/gnode/nix/TestDataArray.java
+++ src/test/java/org/gnode/nix/TestDataArray.java
@@ -101,6 +101,171 @@ public class TestDataArray {
     }
 
     @Test
+    public void testData() {
+
+        NDArray A = new NDArray(new int[]{3, 4, 2}, DataType.Double);
+
+        int values = 0;
+        for (int i = 0; i != 3; ++i)
+            for (int j = 0; j != 4; ++j)
+                for (int k = 0; k != 2; ++k)
+                    A.setDoubleData(new int[]{i, j, k}, values++);
+
+        assertEquals(array1.getDataType(), DataType.Double);
+
+        assertTrue(array1.getDataExtent().equals(new NDSize(new int[]{0, 0, 0})));
+        assertNull(array1.getDimension(1));
+
+        array1.setData(A);
+
+        NDArray B = array1.getData();
+
+        int verify = 0;
+        int errors = 0;
+        for (int i = 0; i != 3; ++i) {
+            for (int j = 0; j != 4; ++j) {
+                for (int k = 0; k != 2; ++k) {
+                    int v = verify++;
+                    if (B.getDoubleData(new int[]{i, j, k}) != v) {
+                        errors += 1;
+                    }
+                }
+            }
+        }
+
+        assertEquals(errors, 0);
+
+        DataArray direct = block.createDataArray("da_direct", "double", A);
+        NDArray Adirect = direct.getData();
+
+        errors = 0;
+        verify = 0;
+        for (int i = 0; i != 3; ++i) {
+            for (int j = 0; j != 4; ++j) {
+                for (int k = 0; k != 2; ++k) {
+                    int v = verify++;
+                    if (Adirect.getDoubleData(new int[]{i, j, k}) != v) {
+                        errors += 1;
+                    }
+                }
+            }
+        }
+
+        assertEquals(errors, 0);
+
+        DataArray directFloat = block.createDataArray("da_direct_int", "int", A, A.getDataType());
+        NDArray AdirectFloat = directFloat.getData();
+
+        errors = 0;
+        verify = 0;
+        for (int i = 0; i != 3; ++i) {
+            for (int j = 0; j != 4; ++j) {
+                for (int k = 0; k != 2; ++k) {
+                    int v = verify++;
+                    if (Adirect.getDoubleData(new int[]{i, j, k}) != v) {
+                        errors += 1;
+                    }
+                }
+            }
+        }
+
+        assertEquals(errors, 0);
+
+        NDArray C = new NDArray(new int[]{5, 5}, DataType.Double);
+
+        for (int i = 0; i != 5; ++i)
+            for (int j = 0; j != 5; ++j)
+                C.setDoubleData(new int[]{i, j}, 42.0);
+
+        assertTrue(array2.getDataExtent().equals(new NDSize(new int[]{20, 20})));
+
+        array2.setData(C);
+        array2.setDataExtent(new NDSize(new int[]{40, 40}));
+        assertTrue(array2.getDataExtent().equals(new NDSize(new int[]{40, 40})));
+
+        NDArray D = new NDArray(new int[]{5, 5}, DataType.Double);
+        for (int i = 0; i != 5; ++i)
+            for (int j = 0; j != 5; ++j)
+                D.setDoubleData(new int[]{i, j}, 42.0);
+
+        array2.setData(D);
+
+        NDArray E = array2.getData();
+
+        for (int i = 0; i != 5; ++i)
+            for (int j = 0; j != 5; ++j)
+                assertTrue(D.getDoubleData(new int[]{i, j}) == E.getDoubleData(new int[]{i, j}));
+
+        NDArray F = array2.getData();
+
+        for (int i = 0; i != 5; ++i)
+            for (int j = 0; j != 5; ++j)
+                assertTrue(D.getDoubleData(new int[]{i, j}) == F.getDoubleData(new int[]{i, j}));
+
+        DataArray da3 = block.createDataArray("direct-vector",
+                "double",
+                DataType.Double,
+                new NDSize(new int[]{5}));
+
+        assertTrue(da3.getDataExtent().equals(new NDSize(new int[]{5})));
+        assertNull(da3.getDimension(1));
+
+        NDArray dv = new NDArray(new double[]{1.0, 2.0, 3.0, 4.0, 5.0}, new int[]{5});
+        da3.setData(dv);
+
+        NDArray dvin = da3.getData();
+
+        for (int i = 0; i < 5; i++) {
+            assertTrue(dv.getDoubleData(new int[]{i}) == dvin.getDoubleData(new int[]{i}));
+        }
+    }
+
+    @Test
+    public void testPolynomial() {
+        double[] coefficients1 = new double[10];
+        double[] coefficients2 = new double[10];
+
+        for (int i = 0; i < 10; i++) {
+            coefficients1[i] = i;
+            coefficients2[i] = i;
+        }
+
+        array2.setPolynomCoefficients(coefficients2);
+        List<Double> ret = array2.getPolynomCoefficients();
+        for (int i = 0; i < ret.size(); i++) {
+            assertTrue(ret.get(i) == coefficients2[i]);
+        }
+
+        array2.setPolynomCoefficients(null);
+        assertTrue(array2.getPolynomCoefficients().size() == 0);
+
+        array2.setExpansionOrigin(3);
+        double retval = array2.getExpansionOrigin();
+        assertTrue(retval == 3);
+        array2.setExpansionOrigin(0.0);
+        assertTrue(array2.getExpansionOrigin() == 0.0);
+
+        //test IO with a polynomial set
+        DataArray dap = block.createDataArray("polyio",
+                "double",
+                DataType.Double,
+                new NDSize(new int[]{2, 3}));
+
+        NDArray dv = new NDArray(new double[]{1.0, 2.0, 3.0, 4.0, 5.0, 6.0}, new int[]{2, 3});
+        dap.setData(dv);
+
+        double[] poly = {1.0, 2.0, 3.0};
+        dap.setPolynomCoefficients(poly);
+
+        NDArray dvin_poly = dap.getData();
+
+        double[] ref = {6, 17, 34, 57, 86, 121};
+        double[] dvin_poly_arr = dvin_poly.getDoubleDataArray();
+
+        assertEquals(ref.length, dvin_poly_arr.length);
+    }
+
+    @Test
     public void testDimension() {
         List<Dimension> dims = new ArrayList<Dimension>();
         double[] ticks = new double[5];
